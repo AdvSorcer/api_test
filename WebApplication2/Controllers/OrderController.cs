5b3df88f0e53af81c1392ef2269419c2ae603b3d
samsonhanku <hanku@samsonglobal.com>
2020-11-11 10:03:13 +0800

    init

---
+++ WebApplication2/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication2.Controllers
+{
+    public class OrderController : ApiController
+    {
+        [HttpGet]
+        public string test()
+        {
+           
+            string connectionString = "Server=192.168.180.215;Database=plm;User Id=innovator;Password=Plmsa@2020*!;";
+            string JSONresult;
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = " SELECT TOP (10) KEYED_NAME,TS_CUSTOMER_ITEM FROM [plm].[innovator].[PART] ";
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.CommandTimeout = 0;
+                connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+
+                da.Fill(dataTable);
+                da.Dispose();
+               
+                JSONresult = JsonConvert.SerializeObject(dataTable);
+            }
+           
+            return JSONresult;
+        }
+    }
+
+}
---
+++ WebApplication2/Controllers/UploadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication2.Controllers
+{
+    public class UploadController : ApiController
+    {
+        [HttpPost]
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public async Task<IHttpActionResult> Upload()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var provider = new MultipartMemoryStreamProvider();
+            await Request.Content.ReadAsMultipartAsync(provider);
+            foreach (var file in provider.Contents)
+            {
+                var filename = file.Headers.ContentDisposition.FileName.Trim('\"');
+                var buffer = await file.ReadAsByteArrayAsync();
+                var saveStream = new MemoryStream(buffer);
+                var filePath = HttpContext.Current.Server.MapPath("~/App_Data/" + filename);
+                using (var fileStream = File.Create(filePath))
+                {
+                    saveStream.CopyTo(fileStream);
+                }
+            }
+
+            return Ok();
+        }
+        [HttpGet]
+        public string test()
+        {
+            return "ok";
+        }
+    }
+}
